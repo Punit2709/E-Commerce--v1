991f4d56db6570f4dea4c67cca2c59e4fbd7e576
=Punit2709 <=punitsp2003@gmail.com>
2024-06-17 23:15:15 +0530

    Protected Route Implemented

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import axios from 'axios';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import WebFont from 'webfontloader'
@@ -30,6 +30,8 @@ import UsersList from './components/Admin/UsersList';
 import UpdateUser from './components/Admin/UpdateUser';
 import ProductReviews from './components/Admin/ProductReviews';
 
+import ProtectedRoute from './components/Routes/ProtectedRoute'
+
 import store from "./store"
 import { loadUser } from "./actions/userAction"
 
@@ -47,7 +49,13 @@ import NotFound from './components/Layout/NotFound/NotFound';
 
 function App() {
 
+  const dispatch = useDispatch();
   const { isAuthenticated, user } = useSelector(state => state.user);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  // if(user.role == 'admin'){
+  //   setIsAdmin(true);
+  // }
 
   const [stripeApiKey, setStripeApiKey] = useState("");
 
@@ -62,46 +70,43 @@ function App() {
         families: ['Roboto', 'Droid sans', 'Chilanka']
       }
     });
-    store.dispatch(loadUser());
+    dispatch(loadUser());
     getStripeApiKey();
   }, []);
 
   return (
     <Router>
       <Header />
-
       <Routes>
-        <Route  path='/' element={<Home />} />
-        <Route  path='/products/details/:id' element={<ProductDetails />} />
-        <Route  path='/products' element={<Products />} />
-        <Route  path='/products/:keyword' element={<Products />} />
-        <Route  path='/search' element={<Search />} />
-        <Route  path='/contact' element={<Contact />} />
-        <Route  path='/about' element={<About />} />
-        <Route  path='/login' element={<LoginSignUp />} />
-        <Route  path='/account' element={<Profile />} />
-        <Route  path='/me/update' element={<UpdateProfile />} />
-        <Route  path='/password/update' element={<UpdatePassword />} />
-        <Route  path='/password/forgot' element={<ForgotPassword />} />
-        <Route  path='/password/reset/:token' element={<ResetPassword />} />
-        <Route  path='/cart' element={<Cart />} />
-        <Route  path='/shipping' element={<Shipping />} />
-        <Route  path='/process/payment' element={<Pay />} />
-        <Route  path='/success' element={<OrderSuccess />} />
-        <Route  path='/orders' element={<MyOrders />} />
-        <Route  path='/order/:id' element={<OrderDetails />} />
-        <Route  path='/order/confirm' element={<ConfirmOrder />} />
-
-        <Route  path='/admin/dashboard' element={<Dashboard />} />
-        <Route  path='/admin/products' element={<ProductList />} />
-        <Route  path='/admin/product' element={<NewProduct />} />
-        <Route  path='/admin/product/:id' element={<UpdateProduct />} />
-        <Route  path='/admin/orders' element={<OrderList />} />
-        <Route  path='/admin/order/:id' element={<ProcessOrder />} />
-        <Route  path='/admin/users' element={<UsersList />} />
-        <Route  path='/admin/user/:id' element={<UpdateUser />} />
-        <Route  path='/admin/reviews' element={<ProductReviews />} />
-        <Route  path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:keyword" element={<Products />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/account" element={<ProtectedRoute element={<Profile />} />} />
+        <Route path="/me/update" element={<ProtectedRoute element={<UpdateProfile />} />} />
+        <Route path="/password/update" element={<ProtectedRoute element={<UpdatePassword />} />} />
+        <Route path="/password/forgot" element={<ForgotPassword />} />
+        <Route path="/password/reset/:token" element={<ResetPassword />} />
+        <Route path="/login" element={<LoginSignUp />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/shipping" element={<ProtectedRoute element={<Shipping />} />} />
+        <Route path="/success" element={<ProtectedRoute element={<OrderSuccess />} />} />
+        <Route path="/orders" element={<ProtectedRoute element={<MyOrders />} />} />
+        <Route path="/order/confirm" element={<ProtectedRoute element={<ConfirmOrder />} />} />
+        <Route path="/order/:id" element={<ProtectedRoute element={<OrderDetails />} />} />
+        <Route path="/admin/dashboard" element={<ProtectedRoute isAdmin={true} element={<Dashboard />} />} />
+        <Route path="/admin/products" element={<ProtectedRoute isAdmin={true} element={<ProductList />} />} />
+        <Route path="/admin/product" element={<ProtectedRoute isAdmin={true} element={<NewProduct />} />} />
+        <Route path="/admin/product/:id" element={<ProtectedRoute isAdmin={true} element={<UpdateProduct />} />} />
+        <Route path="/admin/orders" element={<ProtectedRoute isAdmin={true} element={<OrderList />} />} />
+        <Route path="/admin/order/:id" element={<ProtectedRoute isAdmin={true} element={<ProcessOrder />} />} />
+        <Route path="/admin/users" element={<ProtectedRoute isAdmin={true} element={<UsersList />} />} />
+        <Route path="/admin/user/:id" element={<ProtectedRoute isAdmin={true} element={<UpdateUser />} />} />
+        <Route path="/admin/reviews" element={<ProtectedRoute isAdmin={true} element={<ProductReviews />} />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </Router>
--- Frontend/src/components/Routes/ProtectedRoute.jsx
+++ Frontend/src/components/Routes/ProtectedRoute.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import Loader from "../Layout/Loader/Loader";
 
-function ProtectedRoute() {
-  return (
-    <>
-      Protected Route
-    </>
-  )
-}
 
-export default ProtectedRoute
+const ProtectedRoute = ({ isAdmin, element }) => {
+  const { loading, isAuthenticated, user } = useSelector((state) => {console.log(state); return state.user});
+  console.log(user);
+
+  if (loading) return <Loader />;
+
+  if (!loading && !isAuthenticated) {
+    console.log('Navigating to Login');
+    return <Navigate to="/login" />;
+  }
+
+  if (isAdmin && user.role !== "admin") {
+    return <Navigate to="/account" />;
+  }
+
+  return element;
+};
+
+export default ProtectedRoute;
